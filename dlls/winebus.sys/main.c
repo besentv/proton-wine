ValveSoftware/wine

winebus.sys: Add more trace messages.

dlls/winebus.sys/main.c
@@ -322,6 +322,8 @@ static DEVICE_OBJECT *bus_create_hid_device(const WCHAR *bus_name, struct device
     list_add_tail(&device_list, &ext->entry);
 
     RtlLeaveCriticalSection(&device_list_cs);
+
+    TRACE("created device %p/%p\n", device, unix_device);
     return device;
 }
 
@@ -426,7 +428,7 @@ static BOOL deliver_next_report(struct device_extension *ext, IRP *irp)
 
     if (TRACE_ON(hid))
     {
-        TRACE("read input report length %u:\n", report->length);
+        TRACE("device %p/%p input report length %u:\n", ext->device, ext->unix_device, report->length);
         for (i = 0; i < report->length;)
         {
             char buffer[256], *buf = buffer;

besentv/proton-wine

dlls/winebus.sys/main.c
@@ -322,6 +322,8 @@ static DEVICE_OBJECT *bus_create_hid_device(const WCHAR *bus_name, struct device
     list_add_tail(&device_list, &ext->entry);
 
     RtlLeaveCriticalSection(&device_list_cs);
+
+    TRACE("created device %p/%p\n", device, unix_device);
     return device;
 }
 
@@ -426,7 +428,7 @@ static BOOL deliver_next_report(struct device_extension *ext, IRP *irp)
 
     if (TRACE_ON(hid))
     {
-        TRACE("read input report length %lu:\n", report->length);
+        TRACE("device %p/%p input report length %lu:\n", ext->device, ext->unix_device, report->length);
         for (i = 0; i < report->length;)
         {
             char buffer[256], *buf = buffer;